6d17cc248f9a0796fb2a9747d8f021669a5f0a4b
Sebastian Castiblanco <sebas07casti@gmail.com>
2021-01-21 21:45:19 -0500

    :art: Finish basic pong

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ var game = function(){
     let time =30;
     let movementBar = 20;
     let movement = 20;
-    let witdh = document.documentElement.clientWidth - movement;
+    let width = document.documentElement.clientWidth - movement;
     let height = document.documentElement.clientHeight - movement;
     let controlGame;
     let player1;
@@ -36,7 +36,81 @@ var game = function(){
     }
 
     function play(){
+        moveBall();
         moveBar();
+        checkIfLost();
+    }
+
+    function checkIfLost(){
+        if(ball.offsetLeft >= width){
+            stop();
+            console.log("punto player 1");
+        }
+        if(ball.offsetLeft <= 0){
+            stop();
+            console.log("punto player 2");
+        }
+    }
+
+    function moveBall(){
+        checkStateBall();
+        switch(ball.state){
+            case 1: // derecha, abajo
+                ball.style.left = (ball.offsetLeft + movement) +"px";
+                ball.style.top = (ball.offsetTop + movement) +"px";
+                break;
+            case 2: // derecha, arriba
+                ball.style.left = (ball.offsetLeft + movement) +"px";
+                ball.style.top = (ball.offsetTop - movement) +"px";
+                break;
+            case 3: // izquierda, abajo
+                ball.style.left = (ball.offsetLeft - movement) +"px";
+                ball.style.top = (ball.offsetTop + movement) +"px";
+                break;
+            case 4: // izquierda, arriba
+                ball.style.left = (ball.offsetLeft - movement) +"px";
+                ball.style.top = (ball.offsetTop - movement) +"px";
+                break;
+        }
+    }
+
+    function checkStateBall(){
+
+        if(collidePlayer2()){
+            ball.direction = 2;
+            if(ball.state == 1) ball.state = 3;
+            if(ball.state == 2) ball.state = 4;
+        }else if(collidePlayer1()){
+            ball.direction = 1;
+            if(ball.state == 3) ball.state = 1;
+            if(ball.state == 4) ball.state = 2;
+        }
+
+        if(ball.direction ===1){
+            if(ball.offsetTop >= height) ball.state=2;
+            else if(ball.offsetTop <=0 ) ball.state=1;
+        }else{
+            if(ball.offsetTop >= height) ball.state=4;
+            else if(ball.offsetTop <=0 ) ball.state=3;
+        }
+    }
+
+    function collidePlayer1(){
+        if(ball.offsetLeft <= (bar1.clientWidth) &&
+           ball.offsetTop >= bar1.offsetTop &&
+           ball.offsetTop <= (bar1.offsetTop + bar1.clientHeight)){
+            return true;
+        }
+
+        return false;
+    }
+    function collidePlayer2(){
+        if(ball.offsetLeft >= (width - bar2.clientWidth) &&
+           ball.offsetTop >= bar2.offsetTop &&
+           ball.offsetTop <= (bar2.offsetTop + bar2.clientHeight)){
+            return true;
+        }
+        return false;
     }
 
     function moveBar(){
@@ -56,7 +130,6 @@ var game = function(){
 
     document.onkeydown = function(e){
         e = e || window.event;
-        console.log(e.key);
         switch(e.key){
             case upP1: // Q
             case downP1: // A
